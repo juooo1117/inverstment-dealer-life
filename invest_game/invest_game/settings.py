d5184351e7cdcb537041b5eddcf02113401aeba1
imnarin0316 <imnarin0316@gmail.com>
2023-09-19 22:37:07 +0900

    feat: news migrate

--- invest_game/invest_game/settings.py
+++ invest_game/invest_game/settings.py
@@ -37,6 +37,9 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "news",
+    "quiz",
+    "one_page",
 ]
 
 MIDDLEWARE = [
--- invest_game/news/models.py
+++ invest_game/news/models.py
@@ -1,3 +1,23 @@
 from django.db import models
 
-# Create your models here.
+
+# models는 데이터를 저장하기 위한 하나의 단위
+
+class Post(models.Model):
+    """
+    글쓰기 페이지에 넣을 데이터
+    title : 뉴스피드 타이틀
+    content : 뉴스피드 요약 내용
+    created_at : 작성일
+    author : 작성자 정보
+    """
+
+    # CharField : 최대 길이 정의 필요한 타입
+    # TextField : 그 외
+    title = models.CharField(max_length=30)
+    content = models.TextField()
+
+    created_at = models.DateTimeField()  # 년,월,일,시,초 기록
+    # author = 추후 작성 예정.
+
+    # python manage.py makemigrations : 마이그레이션 등록
