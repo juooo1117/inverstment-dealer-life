5bde4fbbaf695117861d9ffa761c2f3b7bfdc0f3
imnarin0316 <imnarin0316@gmail.com>
2023-09-23 06:04:21 +0900

    feat: news 페이지 프레임 완료

--- invest_game/invest_game/settings.py
+++ invest_game/invest_game/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -40,6 +41,7 @@ INSTALLED_APPS = [
     "news",
     "quiz",
     "one_page",
+    "sass_processor",
 ]
 
 MIDDLEWARE = [
@@ -112,9 +114,22 @@ USE_TZ = False # 시간대 설정
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = "static/"
+STATIC_URL = "/static/"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+
+STATICFILES_FINDERS = [
+    'django.contrib.staticfiles.finders.FileSystemFinder',
+    'django.contrib.staticfiles.finders.AppDirectoriesFinder',
+    'sass_processor.finders.CssFinder',
+]
+STATIC_ROOT = BASE_DIR / 'static'
+SASS_PROCESSOR_ROOT = STATIC_ROOT
+
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
--- invest_game/quiz/views.py
+++ invest_game/quiz/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
 
-# Create your views here.
+def Quiz_list(request):
+    return render(
+        request,
+        'quiz/question.html'
+    )
